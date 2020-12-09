f31c2f97c9cdaadd4ef64e9663c017e194949520
Vincent <vrdphone@gmail.com>
2020-12-09 13:03:08 -0600

    adding files

---
+++ react-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Pokedex from "./components/Pokedex";
+import Pokemon from "./components/Pokemon";
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+      <Route exact path="/:pokemonId" render={(props) => <Pokemon {...props} />} />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ react-pokedex/src/components/Pokedex/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pokedex = () => {
+    return (
+        <div>
+            This is the Pokedex
+        </div>
+    )
+};
+
+export default Pokedex;
